1a5e0f9867bfb088a93001dacbaee472c499898c
Matthias Boeker <matthias.boeker@digdir.no>
2025-04-09 10:01:06 +0200

    Committing 1 item from workspace 7ef8faa8-ed08-4019-a01e-34fec5c0428e

--- projects/internasjonale_aktiviteter/notebook.Notebook/notebook-content.py
+++ projects/internasjonale_aktiviteter/notebook.Notebook/notebook-content.py
@@ -1,4 +1,30 @@
 # Fabric notebook source
 
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   }
+# META }
 
 # CELL ********************
+
+setnence_a = "this is a string"
+
+def foo_bar():
+    return "and this is a string"
+
+setnence_b = foo_bar() 
+print(f"{sentence_a} {sentence_b}")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# ## This is a test notebook
