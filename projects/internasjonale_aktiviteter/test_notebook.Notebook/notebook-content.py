3bbbe9dc1f015a1e0163cf19a0aa91a29afd1c64
matboe-94 <matthias.boeker@digdir.no>
2025-04-10 13:22:32 +0200

    try upload self made notebook

---
+++ projects/internasjonale_aktiviteter/test_notebook.Notebook/notebook-content.py
@@ -0,0 +1,22 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+#Hei hei
+#This is a try out to see if I can make my own notebooks 
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
