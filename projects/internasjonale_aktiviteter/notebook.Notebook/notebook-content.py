bec46f61cad8b9ccf89c077d1a41ee3ec552356b
Matthias Boeker <matthias.boeker@digdir.no>
2025-04-10 13:14:38 +0200

    Committing 2 items from workspace 7ef8faa8-ed08-4019-a01e-34fec5c0428e

--- projects/internasjonale_aktiviteter/notebook.Notebook/notebook-content.py
+++ projects/internasjonale_aktiviteter/notebook.Notebook/notebook-content.py
@@ -5,17 +5,53 @@
 # META {
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "environment": {
+# META       "environmentId": "0c35c7eb-51e7-a8d4-469c-bc6b9a9fa1ca",
+# META       "workspaceId": "00000000-0000-0000-0000-000000000000"
+# META     }
 # META   }
 # META }
 
 # CELL ********************
 
+# Use the 2 magic commands below to reload the modules if your module has updates during the current session. You only need to run the commands once.
+%load_ext autoreload
+%autoreload 2
+import os
+import sys
+#if 'env.src_python.test_function_package' in sys.modules:
+#    del sys.modules['env.src_python.test_function_package']
+
+import env.src_python.test_function_package as test_function_package
+
+# Now import it again
+
+# Try using the function
+sentence_a = "this is a string"
+sentence_b = test_function_package.foo_bar()
+print(f"{sentence_a} {sentence_b}")
+
+#file_path = test_function_package.__file__
+#print(f"File path: {file_path}")
+
+# Read and print the file contents
+#with open(file_path, 'r') as f:
+#    print(f.read())
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
 setnence_a = "this is a string"
-
-def foo_bar():
-    return "and this is a string"
-
-setnence_b = foo_bar() 
+sentence_b = test_function_package.foo_bar()
 print(f"{sentence_a} {sentence_b}")
 
 # METADATA ********************
@@ -25,29 +61,12 @@ print(f"{sentence_a} {sentence_b}")
 # META   "language_group": "synapse_pyspark"
 # META }
 
-# MARKDOWN ********************
-
-# ## This is a test notebook
-
 # CELL ********************
 
-5+5
 
 # METADATA ********************
 
 # META {
 # META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
-# MAGIC %%sparkr
-# MAGIC 10+10
-
-# METADATA ********************
-
-# META {
-# META   "language": "r",
 # META   "language_group": "synapse_pyspark"
 # META }
---
+++ projects/internasjonale_aktiviteter/set-up-data-schemes.Notebook/notebook-content.py
@@ -0,0 +1,25 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+import pandas as pd 
+
+#This is a try out to see how the folder structure in fabric and git sync
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
