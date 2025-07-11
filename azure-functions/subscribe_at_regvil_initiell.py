01dc9b980cb92ba2dbcfcf33b3b3555c784fdf63
matboe-94 <matthias.boeker@digdir.no>
2025-07-11 12:16:29 +0200

    correct subscription to test

--- azure-functions/subscribe_at_regvil_initiell.py
+++ azure-functions/subscribe_at_regvil_initiell.py
@@ -12,7 +12,7 @@ secret = client.get_secret("rapdigtest")
 secret_value = secret.value
 
 def subscribe_to_altinn_events(altinn_token: str, endpoint: str, source_filter: str, type_filter: str):
-    url = "https://platform.altinn.no/events/api/v1/subscriptions"
+    url = "https://platform.tt02.altinn.no/events/api/v1/subscriptions"
     headers = {
        "Authorization": f"Bearer {altinn_token}",
        "Content-Type": "application/json"
@@ -20,7 +20,7 @@ def subscribe_to_altinn_events(altinn_token: str, endpoint: str, source_filter: 
     data = {
        "endPoint": endpoint,
        "sourceFilter": source_filter,
-       "typeFilter": type_filter
+       "typeFilter": type_filter,
     }
     resp = requests.post(url, headers=headers, json=data)
     return resp
@@ -38,13 +38,13 @@ def main():
     maskinport_client = {
     "kid":"RapDig_maskinporten-TEST.2025-05-22",
     "client_id":"387ff46c-222f-412b-ab63-3abf0a2704bc",
-    "scope":"altinn:events.subscribe"
+    "scope":"altinn:serviceowner altinn:events.subscribe"
 }
 
     token = exchange_token(maskinport_client, secret_value, maskinporten_endpoint)
 
     endpoint = "https://regvil-webhook-cmheddg4fvexcfgw.northeurope-01.azurewebsites.net/api/httppost?"
-    source_filter = "https://tt02.apps.altinn.no/tt02/digdir/regvil-2025-initiell"
+    source_filter = "https://digdir.apps.tt02.altinn.no/digdir/regvil-2025-initiell"
     type_filter = "app.instance.process.completed"
     response = subscribe_to_altinn_events(token, endpoint, source_filter, type_filter)
     print("Status Code:", response.status_code)
